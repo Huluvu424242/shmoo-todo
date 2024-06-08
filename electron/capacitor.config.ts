45f32998582c92af8a2a320653ea9e518c5dc8ac
huluvu424242 <huluvu424242@gmail.com>
2024-06-08 21:07:38 +0200

    [projectstruktur] arbeitsstand

--- electron/capacitor.config.ts
+++ electron/capacitor.config.ts
@@ -3,7 +3,12 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.github.huluvu424242.shmooion-todo',
   appName: 'Shmoo Todo',
-  webDir: 'www'
+  webDir: 'www',
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 0
+    }
+  }
 };
 
 export default config;
