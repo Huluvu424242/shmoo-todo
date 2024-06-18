b529411f0c0765fd611686c513280ddc881293ea
Huluvu424242 <huluvu424242@gmail.com>
2024-06-18 22:09:13 +0200

    Feature/orbitdb web (#5)
    orbitdb anbindung per cdn web scripts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
+import {ScriptService} from "./services/ScriptService";
+
+
+declare let Helia: any;
+declare let OrbitDB: any;
+declare let ChainsafeLibp2PGossipsub: any;
+
+declare function greet(): void;
 
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss'],
+    selector: 'app-root',
+    templateUrl: 'app.component.html',
+    styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit {
+    constructor(private scriptService: ScriptService) {
+    }
+
+    async ngOnInit(): Promise<void> {
+        greet();
+        console.log('Loading External Scripts ...');
+        await this.scriptService.load('chainsafe', 'helia', 'orbitdb');
+        await this.startOrbitDB();
+    }
+
+
+    async startOrbitDB(): Promise<void> {
+        const {createHelia, libp2pDefaults} = Helia
+        const {createOrbitDB} = OrbitDB
+        const {gossipsub} = ChainsafeLibp2PGossipsub
+        const libp2pOptions = libp2pDefaults()
+
+        libp2pOptions.services.pubsub = gossipsub()
+
+        const ipfs = await createHelia({libp2p: libp2pOptions})
+        const orbitdb = await createOrbitDB({ipfs})
+        const db = await orbitdb.open('browser')
+        console.log(`OrbitDB Address: ${db.address}`);
+    }
+
+
 }
