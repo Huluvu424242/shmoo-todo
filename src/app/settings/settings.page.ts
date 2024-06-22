4add721ee22a8993189ccb08cf1d8de60bc55e81
huluvu424242 <huluvu424242@gmail.com>
2024-06-22 21:04:40 +0200

    readonly logik implementiert

--- src/app/settings/settings.page.ts
+++ src/app/settings/settings.page.ts
@@ -16,13 +16,27 @@ export class SettingsPage implements OnInit {
 
     protected datenbanken: Map<string, DbAuswahl> = new Map();
 
+    getOrderedDbAuswahlen():string[]{
+        const orderedKeys:string[] = [];
+        orderedKeys.push('new');
+        orderedKeys.push('default');
+        this.datenbanken.forEach((value,key)=>{
+            if(key==='new'|| key==='default'){
+                // diese Keys sind schon enthalten
+            }else{
+                orderedKeys.push(key);
+            }
+        });
+        return orderedKeys;
+    }
+
     database: any;
 
     constructor(private orbitDBService: OrbitDBService) {
+        this.datenbanken.set("office", {name: 'Office', hash: '#3434-83839002-66666'});
         this.datenbanken.set("new", {name: 'New', hash: ''});
         this.datenbanken.set("default", {name: 'Default', hash: '********************'});
         this.datenbanken.set("home", {name: 'Home', hash: '#3434-83839002-28292'});
-        this.datenbanken.set("office", {name: 'Office', hash: '#3434-83839002-66666'});
     }
 
     ngOnInit(): void {
