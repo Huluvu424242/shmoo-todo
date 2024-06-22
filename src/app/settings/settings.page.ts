5ff9b6b112ba8411cf2b4ec58bfacf04ce911edd
huluvu424242 <huluvu424242@gmail.com>
2024-06-22 20:47:18 +0200

    db auswahl logik implementiert

--- src/app/settings/settings.page.ts
+++ src/app/settings/settings.page.ts
@@ -1,28 +1,32 @@
 import {Component, OnInit} from '@angular/core';
 import {OrbitDBService} from "../services/OrbitDBService";
 
+interface DbAuswahl {
+    name: string;
+    hash: string;
+}
+
+
 @Component({
     selector: 'app-settings',
     templateUrl: 'settings.page.html',
     styleUrls: ['settings.page.scss']
 })
-export class SettingsPage implements OnInit{
+export class SettingsPage implements OnInit {
 
+    protected datenbanken: Map<string, DbAuswahl> = new Map();
 
-    // protected auswahl:string ="default";
+    database: any;
 
-    private  auswahlOptionen = new Map<string, string>([
-        ["New", "new"],
-        ["Default", "default"]
-    ]);
-
-
-    database:any;
     constructor(private orbitDBService: OrbitDBService) {
+        this.datenbanken.set("new", {name: 'New', hash: ''});
+        this.datenbanken.set("default", {name: 'Default', hash: '********************'});
+        this.datenbanken.set("home", {name: 'Home', hash: '#3434-83839002-28292'});
+        this.datenbanken.set("office", {name: 'Office', hash: '#3434-83839002-66666'});
     }
 
     ngOnInit(): void {
-       this.database=this.orbitDBService.getDB();
+        this.database = this.orbitDBService.getDB();
     }
 
 }
